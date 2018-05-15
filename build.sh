38fb6696c17596d359046d0467d83c1f1ff7dfb9
Lucas Vanhalst <lucas.vanhalst@gmail.com>
2018-05-15 14:14:39 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 docker-compose up -d grafana
-mv monitoring/parser/data .
+mv monitoring/parser/data /tmp
 docker-compose up -d --build log_parser
-mv data monitoring/parser
+mv /tmp/data monitoring/parser
 docker-compose up -d backup
